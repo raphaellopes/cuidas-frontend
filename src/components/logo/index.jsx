7d07d502d3530ef765fdc19ef23267ca68325009
Raphael Lopes <ph.campanario07@gmail.com>
2019-06-12 17:14:06 -0300

    Create logo component

---
+++ src/components/logo/index.jsx
@@ -0,0 +1,12 @@
+// vendors
+import React from 'react';
+
+// locals
+import LogoSvg from '../../assets/images/logo.svg';
+import { LogoStyle } from './styles';
+
+const Logo = () => (
+  <LogoStyle src={LogoSvg} alt="Cuidas" />
+);
+
+export default Logo;
---
+++ src/components/logo/styles.js
@@ -0,0 +1,6 @@
+// vendors
+import styled from 'styled-components';
+
+export const LogoStyle = styled.img`
+  height: 28px;
+`;
