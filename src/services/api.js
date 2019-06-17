eeb3e3691ef8c1a8c83492df7c8b6357db919668
Raphael Lopes <ph.campanario07@gmail.com>
2019-06-17 19:45:33 -0300

    Add API URL by environment var

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // @TODO: Create config vars by environment
-  baseURL: 'http://localhost:3001',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
