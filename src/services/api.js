4e72f0b6399ac92949e9966d4ebfc42992a5565c
Raphael Lopes <ph.campanario07@gmail.com>
2019-06-18 11:14:18 -0300

    Fix yarn script and api utl

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://cuidas.herokuapp.com',
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://cuidas.herokuapp.com',
 });
 
 export default api;
